fc9d895d290cc3f200f801b01f7e5124757050a0
mmunozc <mateomunozc@outlook.com>
2025-09-10 16:45:44 -0500

    fix deploy config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,13 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideClientHydration(),
     provideHttpClient(withFetch()),
     provideAnimations() 
